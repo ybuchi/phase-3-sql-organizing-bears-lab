85482c4eb3748a1030e427640dd4dd24c8370d6c
ybuchi <yabuch_ch@hotmail.com>
2022-04-07 15:46:07 -0400

    commit tree d4cedc80fab1f5afb07091c8e61671e703ef6581

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ("Mr. Chocolate", 5, "male", "brown", "playful", true),
+    ("Rowdy", 23, "male", "brown", "fierce", true),
+    ("Tabitha", 10, "female", "white", "restless", true),
+    ("Sergeant Brown", 50, "male", "black", "sleepy", false),
+    ("Melissa", 11, "female", "red", "hungry", true ),
+    ("Grinch", 9, "male", "green", "plotting", true),
+    ("Wendy", 60, "female", "white", "soft", false);
+
+INSERT INTO bears (age, sex, color, temperament, alive)
+VALUES
+    (1, "female", "black", "baby", true);
